7eb064bf1282ad649185c55433e87756295544ce
Bibil Mathew Chacko <bibil.mathew@mindlabssolutions.com>
2022-05-11 20:40:26 +0530

    first commit

---
+++ src/main/java/com/mindlabssolutions/pbtskerala/PbtskeralaApplication.java
@@ -0,0 +1,13 @@
+package com.mindlabssolutions.pbtskerala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PbtskeralaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PbtskeralaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mindlabssolutions/pbtskerala/PbtskeralaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindlabssolutions.pbtskerala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PbtskeralaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
